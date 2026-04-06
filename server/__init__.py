8a55837829e68f3d473441d7d4a87459adcfa2ef
peter mj <petermj2222@gmail.com>
2026-04-06 18:26:48 +0530

    Fix __init__.py import path

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,5 @@
 """Medical Prescription Validation Environment - Server Components."""
 
-from .prescription_validator_environment import PrescriptionValidationEnvironment
+from .environment import PrescriptionValidationEnvironment
 
 __all__ = ["PrescriptionValidationEnvironment"]
